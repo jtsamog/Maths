96eaa3814de397143dde052a4edbf31a231b4039
Sam Ogunyowa <mailsamog@gmail.com>
2017-06-27 13:40:59 -0400

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Endeavour2 on 6/27/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
